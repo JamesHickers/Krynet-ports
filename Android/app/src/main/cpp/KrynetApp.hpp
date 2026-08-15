120eae8ebf748337e08394898a8678261a3ee7c6
Barney (Kyle) <130295277+JamesHickers@users.noreply.github.com>
2026-08-14 21:16:16 -0400

    Create KrynetApp.hpp

---
+++ Android/app/src/main/cpp/KrynetApp.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+class KaiBridge;
+class SciterView;
+
+class KrynetApp
+{
+public:
+    KrynetApp();
+    ~KrynetApp();
+
+    bool initialize();
+    void shutdown();
+
+    SciterView* view() const;
+
+private:
+    KaiBridge* kai = nullptr;
+    SciterView* sciter = nullptr;
+
+    bool initialized = false;
+};
